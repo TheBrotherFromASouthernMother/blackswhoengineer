57da5e658769e1ce6a7b10d02fe6dae4265b5cb7
E. Edgeworth <edgeworth@blackswho.engineer>
2019-11-04 07:40:03 -0800

    remove unused variables and newer submit text
    Variable changes suggested in PR, and I like the new
    submit text better, so getting it back in.

--- src/pages/nominate.js
+++ src/pages/nominate.js
@@ -53,8 +53,6 @@ class App extends React.Component {
   handleSubmit = e => {
     const validation = this.validateName();
     if (!validation && window.confirm('This form will take you to Twitter https://twitter.com/ in order to send your nomination')) {
-      const urlEncodedTwitterHandle = encode(this.state.name);
-      const urlEncodedReason = encode(this.state.reason);
       const directMessageLink =
         `https://twitter.com/messages/compose?text=Please+consider+following+${this.state.name}+because+${this.state.reason}&recipient_id=1129894370339983360`
 
@@ -129,7 +127,7 @@ class App extends React.Component {
                 disabled={this.state.nameValidationMessage}
               >
                 <span className={styles.jobButton}>
-                  Submit
+                  Send us a Twitter DM!
                   <span className={styles.arrow}>→</span>
                 </span>
               </button>
